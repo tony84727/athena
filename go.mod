4452ff94ef75579f507130b5d83fbd9ce2528d8a
tony84727 <tony84727@gmail.com>
2019-09-15 19:15:18 +0800

    add go server

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tony84727/athena
+
+go 1.13
+
+require google.golang.org/grpc v1.23.1 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"github.com/tony84727/athena/forgegrpc"
+	"google.golang.org/grpc"
+	"log"
+)
 
+func main() {
+	client, err := grpc.Dial("localhost:30000", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	chat := forgegrpc.NewChatClient(client)
+	ctx := context.Background()
+	connect, err := chat.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		e, err := connect.Recv()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("sender: %s, message: %s\n", e.Sender, e.Content)
+	}
 }
