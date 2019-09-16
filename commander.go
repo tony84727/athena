ff25a8a799b644f63b5bb1fb5efbe1e88aeae296
tony84727 <tony84727@gmail.com>
2019-09-16 23:31:23 +0800

    add Commander

---
+++ commander.go
@@ -0,0 +1,44 @@
+package main
+
+import "regexp"
+
+var commandPattern = regexp.MustCompile(`^!(\S+)\s+(.*)`)
+
+type Command interface {
+	Run(out chan<-string, args string)
+}
+
+func NewCommander() *Commander {
+	return &Commander{
+		commands: make(map[string]Command),
+	}
+}
+
+type Commander struct {
+	commands map[string]Command
+}
+
+func (c *Commander) RegisterCommand(name string, command Command) {
+	c.commands[name] = command
+}
+
+func (c *Commander) Run(input <-chan string, out chan<- string) {
+	for message := range input {
+		matches := commandPattern.FindStringSubmatch(message)
+		if matches != nil {
+			cmd, exist := c.commands[matches[0]]
+			if exist {
+				commandOutput := make(chan string)
+				go func() {
+					for s := range commandOutput {
+						out <- s
+					}
+				}()
+				go func() {
+					defer close(commandOutput)
+					cmd.Run(out, matches[1])
+				}()
+			}
+		}
+	}
+}
